cec5d09d245f2ef74279f0a3446582aaae31ed04
Kim0426 <706shin1728@naver.com>
2024-06-16 23:32:13 +0900

    chore: setting vitest environment, add @types

---
+++ @types/helper/index.ts
@@ -0,0 +1,7 @@
+export type Equal<A, B> = A extends B ? (B extends A ? 1 : 0) : 0;
+
+const PASS = 1;
+
+export declare function check<A, B>(
+  params: Equal<Equal<A, B>, typeof PASS>
+): void;
---
+++ @types/primitive/index.ts
@@ -0,0 +1,10 @@
+export type Primitive =
+  | string
+  | number
+  | boolean
+  | symbol
+  | null
+  | undefined
+  | bigint;
+
+export type Falsy = false | 0 | '' | null | undefined;
